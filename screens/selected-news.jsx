8c9097d515ba09c0abb546faad26f82df15879d0
MrFreeman <astrauh1.dima1@ya.ru>
2020-10-01 12:08:47 +0300

    add share link function

--- screens/selected-news.jsx
+++ screens/selected-news.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Share } from 'react-native';
 import { WebView } from 'react-native-webview';
+import styled from 'styled-components/native';
+import { Entypo } from '@expo/vector-icons';
+import { MAIN_BUTTON_TEXT_COLOR } from './../style-variables'; 
 
 export const SelectedNews = ({ route: { params: { link } } }) => {
-  return <WebView source={{ uri: link }}/>
+  const onShare = async () => {
+    try {
+      const result = await Share.share({ message: link });
+      if (result.action === Share.dismissedAction) {
+        alert('Не удалось поделиться, потворите попытку позже');
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <Fragment>
+      <ShareButton onPress={onShare}>
+        <Entypo name="share" size={36} color={MAIN_BUTTON_TEXT_COLOR} />
+      </ShareButton>
+      <WebView source={{ uri: link }}/>
+    </Fragment>
+  ) 
 }
+
+const ShareButton = styled.TouchableOpacity`
+  position: absolute;
+  z-index: 99;
+  top: -47px;
+  right: 10px;
+`;
